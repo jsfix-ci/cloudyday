8642c3f237a9ef69d818289914014e07d56de412
zephinzer <dev@joeir.net>
2017-01-10 18:42:36 +0800

    Adds injectTapEventPlugin()

--- webview/index.js
+++ webview/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
 
 import ApplicationRoot from './application-root';
 
